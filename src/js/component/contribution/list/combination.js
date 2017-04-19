7fa75db2671271968c7ab0a08e1daae2d1847899
“wheatandcat” <wheatandcat@gmail.com>
2017-04-20 03:38:19 +0900

    list リファクタリング

---
+++ src/js/component/contribution/list/combination.js
@@ -0,0 +1,50 @@
+// @flow
+import PropTypes from "prop-types"
+import React, { Component } from "react"
+import {List, Open, Close} from "./"
+
+export default class Combination extends Component {
+  render() {
+    let list = []
+
+    this.props.List.forEach((item) => {
+      let Bottom
+
+      if (item.ID == this.props.OpenID) {
+        Bottom = (
+          <Open
+            ID={item.ID}
+            Title={item.title}
+            onDelete={this.props.onDelete}
+          >
+            {this.props.Show}
+          </Open>
+        )
+      } else {
+        Bottom = (
+          <Close
+            ID={item.ID}
+            onAdd={this.props.onAdd}
+          />
+        )
+      }
+
+      list.push({
+        Content: item,
+        Bottom,
+      })
+    })
+
+    return (
+      <List List={list} />
+    )
+  }
+}
+
+Combination.propTypes = {
+  List: PropTypes.array,
+  OpenID: PropTypes.number,
+  Show: PropTypes.element,
+  onAdd: PropTypes.func,
+  onDelete: PropTypes.func,
+}
---
+++ src/js/component/contribution/list/combination.test.js
@@ -0,0 +1,82 @@
+// @flow
+import React from "react"
+import {shallow} from "enzyme"
+import Combination from "./combination"
+
+function setup() {
+  const props = {
+    List: [
+      {
+        FollowCount: 2,
+        ID: 1,
+        Movie: {
+          movie_id: "test",
+          movie_status: 1,
+        },
+        Search: "abctestedfgtuk\njshajkhfkznfklj\njdhjksdhjdklh",
+        OnSearch: jest.fn(),
+        SearchMatch: "test",
+        SoundStatus: 0,
+        Tag: [
+          {
+            ID: 1,
+            Name: "tag1",
+            UserContributionID: 2,
+          }
+        ],
+        Title: "test",
+        UpdatedAt: "2015-01-01T07:12:00+09:00",
+        User: {
+          ID: 1,
+          Name: "name",
+          ProfileImageID: 0,
+        },
+        ViewStatus: 1,
+      },
+      {
+        FollowCount: 2,
+        ID: 2,
+        Movie: {
+          movie_id: "test2",
+          movie_status: 1,
+        },
+        Search: "abctestedfgtuk\njshajkhfkznfklj\njdhjksdhjdklh",
+        OnSearch: jest.fn(),
+        SearchMatch: "test",
+        SoundStatus: 0,
+        Tag: [
+          {
+            ID: 1,
+            Name: "tag1",
+            UserContributionID: 2,
+          }
+        ],
+        Title: "test",
+        UpdatedAt: "2015-01-01T07:12:00+09:00",
+        User: {
+          ID: 1,
+          Name: "name",
+          ProfileImageID: 0,
+        },
+        ViewStatus: 1,
+      },
+    ],
+    OpenID: 1,
+    Show: (<div>aaaaa</div>),
+    onAdd: jest.fn(),
+    onDelete: jest.fn(),
+  }
+
+  const enzymeWrapper = shallow(<Combination {...props} />)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+describe("components/contribution/list/combination", () => {
+  it("表示", () => {
+    setup()
+  })
+})
