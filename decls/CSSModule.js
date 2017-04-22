0e1bca44e1aebd168d6e8d9151e95b7371b5c36a
“wheatandcat” <wheatandcat@gmail.com>
2017-04-23 05:23:07 +0900

    update flow

---
+++ decls/CSSModule.js
@@ -0,0 +1,17 @@
+// @flow
+declare module CSSModule {
+  declare var Alert: string
+  declare var Balloon: string
+  declare var Body: string
+  declare var Dark: string
+  declare var Disable: string
+  declare var Follow: string
+  declare var Gap: string
+  declare var Group: string
+  declare var Image: string
+  declare var LittleStrong: string
+  declare var LittleStrong: string
+  declare var Strong: string
+  declare var Talk: string
+  declare var Thin: string
+}
