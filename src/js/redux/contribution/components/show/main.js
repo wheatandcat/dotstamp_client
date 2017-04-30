d4846a43f14393aa29d1104b79324a47c867ae7b
“wheatandcat” <wheatandcat@gmail.com>
2017-05-01 00:13:04 +0900

    add compnent

--- src/js/redux/contribution/components/show/main.js
+++ src/js/redux/contribution/components/show/main.js
@@ -1,17 +1,8 @@
 import PropTypes from "prop-types"
 import React, { Component } from "react"
-import {Label, Well, Radio, FormGroup, Modal, Dropdown, MenuItem, Grid, Row, Col, Button, PageHeader, Glyphicon} from "react-bootstrap"
-
 import ContributionShowFrame from "./frame"
-import {PROBLEM_TYPE_SPAM, PROBLEM_TYPE_INAPPROPRIATE} from "../../../../constants/contribution"
 import Footer from "../../../../utils/parts/footer"
-import {FontNormal, Shift, HalfTop, Middle, Center, Large, Info, Paragraph, Gap} from "../../../../../css/common.css"
-import {Author} from "../../../../../css/contribution.css"
-
-import YouTube from "../../../../utils/youtube"
-import Icon from "../../../../utils/parts/icon"
-import Tag from "../../../../utils/parts/tag"
-import {DateTimeFormat} from "../../../../utils/common"
+import {Header, Follow, Problem} from "../../../../component/contribution/show/"
 
 export default class Main extends Component {
   componentWillMount() {
@@ -57,36 +48,6 @@ export default class Main extends Component {
     this.props.deleteFollow({userContributionId: this.getID()})
   }
   /**
-   * フォローを取得する
-   */
-  getFollow() {
-    if (!this.props.contributionShow.Load) {
-      return ""
-    }
-
-    if (!this.props.loginAuth.Login) {
-      return (
-        <Button bsStyle="success" bsSize="small" disabled block>
-          <Glyphicon glyph="thumbs-up"/>&nbsp;フォローする(ゲストユーザーは使用できません)
-        </Button>
-      )
-    }
-
-    if (!this.props.contributionShow.Following) {
-      return (
-        <Button bsStyle="success" bsSize="small" block onClick={() => this.addFollow()}>
-          <Glyphicon glyph="thumbs-up"/>&nbsp;フォローする
-        </Button>
-      )
-    }
-
-    return (
-      <Button bsStyle="success" bsSize="small" block onClick={() => this.deleteFollow()}>
-        <Glyphicon glyph="thumbs-up"/>&nbsp;フォロー済み
-      </Button>
-    )
-  }
-  /**
    * 通報する
    */
   addProblem() {
@@ -97,163 +58,6 @@ export default class Main extends Component {
     this.props.addProblem({userContributionId: this.props.contributionShow.ID, type: this.props.contributionShow.ProblemType})
   }
   /**
-   * 通報を取得する
-   */
-  getProblem() {
-    if (this.getID() == 0) {
-      return
-    }
-
-    var send = ""
-    if (this.props.contributionShow.AddProblem) {
-      send = (
-        <div>
-          <br/>
-          <Well>
-            通報を送信しました。ご協力ありがとうございます。
-          </Well>
-        </div>
-      )
-    }
-
-    return (
-      <Modal show={this.props.contributionShow.Problem} onHide={this.props.closeProblem}>
-        <Modal.Header closeButton>
-          <Modal.Title>投稿を通報する</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>以下の理由で通報します。</h4>
-          <br/>
-          <div className={Shift}>
-            <FormGroup>
-              <Radio
-                name="problemType"
-                value={PROBLEM_TYPE_SPAM}
-                checked={this.props.contributionShow.ProblemType == PROBLEM_TYPE_SPAM}
-                readOnly={this.props.contributionShow.ProblemType == PROBLEM_TYPE_SPAM}
-                onChange={() => this.props.setProblemType(PROBLEM_TYPE_SPAM)}
-              >
-                スパムです
-              </Radio>
-              <Radio
-                name="problemType"
-                value={PROBLEM_TYPE_INAPPROPRIATE}
-                checked={this.props.contributionShow.ProblemType == PROBLEM_TYPE_INAPPROPRIATE}
-                readOnly={this.props.contributionShow.ProblemType == PROBLEM_TYPE_INAPPROPRIATE}
-                onChange={() => this.props.setProblemType(PROBLEM_TYPE_INAPPROPRIATE)}
-              >
-                不適切な内容が含まれています
-              </Radio>
-            </FormGroup>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button bsStyle="success" onClick={() => this.addProblem()}>
-            <Glyphicon glyph="send"/>&nbsp;送信
-          </Button>
-          {send}
-        </Modal.Footer>
-      </Modal>
-    )
-
-  }
-  /**
-   * タイトルを取得する
-   */
-  getTitle() {
-    if (this.props.contributionShow.User == undefined) {
-      return ""
-    }
-
-    let tagList = this.props.contributionShow.TagList
-    if (!Array.isArray(tagList)) {
-      tagList = []
-    }
-
-    let followCount = this.props.contributionShow.FollowCount
-    if (this.props.contributionShow.FollowCount < 0) {
-      followCount = ""
-    }
-
-    return (
-      <div>
-        {this.getProblem()}
-        <Grid>
-          <Row className="show-grid">
-            <Col xs={12} md={8}>
-              <PageHeader>{this.props.contributionShow.Title}</PageHeader>
-              <div>
-                <Tag list={tagList}/>
-              </div>
-            </Col>
-            <Col xsHidden md={4}>
-              <div className={Center}>
-                <Glyphicon glyph="thumbs-up"/>
-                <span className={Large}>&nbsp;{followCount}</span>
-              </div>
-              {this.getFollow()}
-            </Col>
-          </Row>
-        </Grid>
-        <br/>
-        <div className={Info}>
-          <Grid>
-            <Row>
-              <Col sm={2} md={1} className={Paragraph}>
-                <Icon imageId={this.props.contributionShow.User.ProfileImageID}/>
-              </Col>
-              <Col sm={18} md={10} className={Author + " " + Gap}>
-                <div className={Middle}>
-                  {this.props.contributionShow.User.Name}
-                  &nbsp;&nbsp;&nbsp;<Glyphicon glyph="time"/>
-                  &nbsp;{DateTimeFormat(this.props.contributionShow.UpdatedAt)}に更新
-                </div>
-              </Col>
-              <Col md={1} xsHidden>
-                <div className={"pull-right " + HalfTop}>
-                  <Dropdown id="dropdown-custom-1" pullRight>
-                    <Dropdown.Toggle noCaret>
-                      <Glyphicon glyph="list"/>
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                      <MenuItem eventKey="1" onClick={() => this.props.openProblem()}>
-                        <Glyphicon glyph="warning-sign"/>&nbsp;この記事を通報する
-                      </MenuItem>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </div>
-              </Col>
-            </Row>
-          </Grid>
-        </div>
-        <br/>
-      </div>
-    )
-  }
-  /**
-   * YouTubeを取得する
-   *
-   * @return {object} html
-   */
-  getYoutube() {
-    if (this.props.contributionShow.Movie.movie_id == "") {
-      return ""
-    }
-
-    return (
-      <div className="container">
-        <Label
-          bsStyle="success"
-          className={FontNormal}
-        >
-          記事を読み上げる
-        </Label>
-        <br/>
-        <YouTube videoId={this.props.contributionShow.Movie.movie_id}/>
-      </div>
-    )
-  }
-  /**
    * 描画する
    *
    * @return {object} html
@@ -261,8 +65,32 @@ export default class Main extends Component {
   render() {
     return (
       <div>
-        {this.getTitle()}
-        {this.getYoutube()}
+        <Problem
+          problemType={this.props.contributionShow.ProblemType}
+          send={this.props.contributionShow.AddProblem}
+          show={this.props.contributionShow.Problem}
+          onProblemType={this.props.setProblemType}
+          onHide={this.props.closeProblem}
+          onAdd={this.addProblem.bind(this)}
+        />
+        <Header
+          followElement={(
+            <Follow
+              count={this.props.contributionShow.FollowCount}
+              actived={(this.props.contributionShow.Following)}
+              disabled={(this.props.loginAuth.Login)}
+              onAdd={this.addFollow}
+              onDelete={this.deleteFollow}
+            />
+          )}
+          tagList={this.props.contributionShow.TagList}
+          title={this.props.contributionShow.Title}
+          profileImageID={this.props.contributionShow.User.ProfileImageID}
+          userName={this.props.contributionShow.User.Name}
+          updatedAt={this.props.contributionShow.UpdatedAt}
+          movieID={this.props.contributionShow.Movie.movie_id}
+          openProblem={this.props.openProblem}
+        />
         <ContributionShowFrame
           title={this.props.contributionShow.Title}
           body={this.props.contributionShow.Body}/>
