a5ce2ffcf17c37870897f28fe3a714bdfc4fe8a2
“wheatandcat” <wheatandcat@gmail.com>
2017-05-03 01:10:44 +0900

    add hmr

---
+++ src/js/menu.js
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types"
+import React, { Component } from "react"
+import ErrorShow from "./redux/error/containers/show"
+import Header from "./utils/parts/header"
+
+export default class Menu extends Component {
+  render () {
+    return (
+      <div>
+        <Header/>
+        {this.props.children}
+        <ErrorShow />
+      </div>
+    )
+  }
+}
+
+Menu.propTypes = {
+  children: PropTypes.object,
+}
