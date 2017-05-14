e846beffe67e883cb87418a910bce6be8ece000f
“wheatandcat” <wheatandcat@gmail.com>
2017-05-14 17:04:57 +0900

    add login

---
+++ .storybook/container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MemoryRouter } from "react-router"
+import { Well } from "react-bootstrap"
+
+export const Container = (story) => (
+  <MemoryRouter initialEntries={["/"]}>
+    <Well>
+      <div className="container">
+        {story()}
+      </div>
+    </Well>
+  </MemoryRouter>
+)
